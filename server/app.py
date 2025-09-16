fb6a198ed91be1b401327c5378bf3c922925b147
Ryan Langat Jnr <ryanlangatjnr@gmail.com>
2025-09-17 01:32:02 +0300

    done lab

--- server/app.py
+++ server/app.py
@@ -1,55 +1,70 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 from models import db, Plant
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
+# Configure database URI based on environment
+if os.environ.get('TESTING') == 'True':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+CORS(app)
 db.init_app(app)
 
-api = Api(app)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
+@app.route('/')
+def index():
+    return '<h1>Plant Server</h1>'
 
-class Plants(Resource):
+# GET /plants route
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants])
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+# GET /plants/:id route
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get(id)
+    if plant:
+        return jsonify(plant.to_dict())
+    return make_response(jsonify({"error": "Plant not found"}), 404)
 
-    def post(self):
-        data = request.get_json()
+# PATCH /plants/:id route - Update plant
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+    
+    data = request.get_json()
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+    
+    db.session.commit()
+    return jsonify(plant.to_dict())
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+# DELETE /plants/:id route - Delete plant
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+    
+    db.session.delete(plant)
+    db.session.commit()
+    return make_response('', 204)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,16 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
+class Plant(db.Model):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255))
     price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
-
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    is_in_stock = db.Column(db.Boolean, default=True)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price,
+            'is_in_stock': self.is_in_stock
+        }
